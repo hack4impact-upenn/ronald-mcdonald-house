43ce692419ec5ff77ae7229ce5da3d6d3bdef1cd
Jasmine Lee <leej@Jasmines-MacBook-Pro-3.local>
2019-10-19 11:22:03 -0400

    temp commit

--- app/room_request/views.py
+++ app/room_request/views.py
@@ -2,8 +2,15 @@ from flask import (
     Blueprint,
 )
 
+from .forms import RoomRequestForm
+from . import room_request
+
 room_request = Blueprint('room_request', __name__)
 
 # TODO dashboard at route /
 
 # TODO create form at route /new
+@room_request.route('/new')
+def new():
+    """New Room Request page."""
+    
