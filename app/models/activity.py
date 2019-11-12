f0dae9f5612727adbfde043401e763649e5c8866
Katie Jiang <kajiang@seas.upenn.edu>
2019-11-11 21:14:28 -0500

    Add activity fake data, edit columns

--- app/models/activity.py
+++ app/models/activity.py
@@ -1,31 +1,50 @@
 from .. import db
 from sqlalchemy import Column, Integer, Boolean, DateTime
-from .user import User
-from .room_request import *
 from datetime import datetime
+from .user import User
 
 
-class  Activity(db.Model):
-    _tablename_ = "activty"
+class Activity(db.Model):
+    _tablename_ = "activity"
 
     id = db.Column(db.Integer, primary_key=True)
-    created_date = db.Column(DateTime, default= datetime.utcnow)    
+    created_at = db.Column(DateTime, default=datetime.utcnow)
     text = db.Column(db.String())
-    commenter = db.Column(db.String())
-    #forgin keys for user and 
+    
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     room_request_id = db.Column(db.Integer, db.ForeignKey('room_requests.id'))
-    
-    users = db.relationship('User', backref= ('activty'))
-    room = db.relationship('RoomRequest', backref='activty')
-
 
     def __repr__(self):
         return self.text
 
-    def print_info(self):
-        print(f'<Last Activty was at: {self.created_at}. {self.text}>')
-
     def __str__(self):
         return self.__repr__()
+
+    def print_info(self):
+        print('<Activity: \n'
+              f'created_at: {self.created_at}'
+              f'text: {text}'
+              f'user_id: {self.user_id}'
+              f'user_name: {self.user.first_name} {self.user.last_name}'
+              f'room_request_id: {self.room_request_id}')
     
+    def generate_fake(room_request, **kwargs):
+        """Generates fake activity for the given room request for testing."""
+        from sqlalchemy.exc import IntegrityError
+        from faker import Faker
+        import random
+
+        fake = Faker()
+        random.seed()
+        users = User.query.all()
+        for i in range(random.randint(1, 10)):
+            user = random.choice(users)
+            activity = Activity(
+                text=fake.paragraph(),
+                user_id=user.id,
+                room_request_id=room_request.id)
+            db.session.add(activity)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
