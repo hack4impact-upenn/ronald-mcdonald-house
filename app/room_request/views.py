3e1d2643e53035308f840187128c8d3b69a0fc3f
Katie Jiang <katiesjiang@gmail.com>
2019-11-15 08:48:28 -0500

    View & manage room request at /room-request/<id>/manage (#18)
    * Allow logged in users to manage individual room requests
    
    * Fix typo

--- app/room_request/views.py
+++ app/room_request/views.py
@@ -7,6 +7,8 @@ from flask import (
     url_for,
 )
 from flask_login import current_user, login_required
+from flask_rq import get_queue
+from app import db
 
 import os
 import datetime
@@ -14,21 +16,109 @@ import urllib
 
 import sqlalchemy
 from sqlalchemy import create_engine, not_, or_
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import sessionmaker
 
-from app import db
-from app.models import Activity, EditableHTML, Role, RoomRequest, User
-from ..decorators import admin_required
 from .forms import RoomRequestForm, ActivityForm, TransferForm
+from .helpers import get_room_request_from_form get_form_from_room_request
+from ..decorators import admin_required
+from ..email import send_email
+from ..models import Activity, EditableHTML, RoomRequest, User, Role
 
 room_request = Blueprint('room_request', __name__)
 
+
 @login_required
-@room_request.route('/', methods=['GET', 'POST'])
-def manage():
+@room_request.route('/')
+def dashboard():
     """View all room requests."""
     room_requests = RoomRequest.query.all()
-    return render_template('room_request/manage.html', room_requests=room_requests)
+    return render_template('room_request/dashboard.html', room_requests=room_requests)
+
+
+@login_required
+@room_request.route('/<int:id>/manage')
+@room_request.route('/<int:id>/info')
+def manage(id):
+    """Manage room request."""
+    room_request = RoomRequest.query.get(id)
+    if room_request is None:
+        return abort(404)
+    return render_template('room_request/manage.html', room_request=room_request)
+
+
+@login_required
+@room_request.route('/<int:id>/patient')
+def patient_info(id):
+    """View patient info of given room request."""
+    room_request = RoomRequest.query.get(id)
+    if room_request is None:
+        return abort(404)
+    return render_template('room_request/manage.html', room_request=room_request)
+
+
+@login_required
+@room_request.route('/<int:id>/room-occupancy')
+def room_occupancy_info(id):
+    """View room occupancy needs for given room request."""
+    room_request = RoomRequest.query.get(id)
+    if room_request is None:
+        return abort(404)
+    return render_template('room_request/manage.html', room_request=room_request)
+
+
+@login_required
+@room_request.route('/<int:id>/guests')
+def guest_info(id):
+    """View table of guests for given room request."""
+    room_request = RoomRequest.query.get(id)
+    if room_request is None:
+        return abort(404)
+    return render_template('room_request/manage.html', room_request=room_request)
+
+
+@login_required
+@room_request.route('/<int:id>/edit')
+def edit(id):
+    """Edit room request info."""
+    room_request = RoomRequest.query.get(id)
+    if room_request is None:
+        return abort(404)
+    
+    form = get_form_from_room_request(room_request)
+    if form.validate_on_submit():
+        room_request = get_room_request_from_form(form)
+        try:
+            db.session.add(room_request)
+            db.session.commit()
+            flash('Successfully saved changes.', 'form-success')
+        except IntegrityError:
+            db.session.rollback()
+            flash('Unable to save changes. Please try again.', 'form-error')
+    return render_template('room_request/manage.html', room_request=room_request, form=form)
+
+
+@login_required
+@room_request.route('<int:id>/delete')
+def delete(id):
+    """Request deletion of a room request, but does not actually perform the action until user confirmation."""
+    room_request = RoomRequest.query.get(id)
+    if room_request is None:
+        return abort(404)
+    return render_template('room_request/manage.html', room_request=room_request)
+
+
+@login_required
+@room_request.route('<int:id>/_delete')
+def _delete(id):
+    """Delete a room request."""
+    room_request = RoomRequest.query.get(id)
+    if room_request:
+        db.session.delete(room_request)
+        db.session.commit()
+        flash(f'Successfully deleted room request for {room_request.first_name} {room_request.last_name}.', 'success')
+    return redirect(url_for('room_request.dashboard'))
+
 
 @login_required
 @room_request.route('/new', methods=['GET', 'POST'])
@@ -37,71 +127,23 @@ def new():
     editable_html_obj = EditableHTML.get_editable_html('room_request')
     form = RoomRequestForm()
     if form.validate_on_submit():
-        room_request = RoomRequest(
-            first_name = form.first_name.data,
-            last_name = form.last_name.data,
-            relationship_to_patient = form.rel_to_patient.data,
-            address_line_one = form.street_address.data,
-            address_line_two = form.apt_st_address.data,
-            city = form.city.data,
-            state = form.state.data,
-            zip_code = form.zipcode.data,
-            country = form.country.data,
-            primary_phone = form.phone_number.data,
-            secondary_phone = form.alt_phone_number.data,
-            email = form.email.data,
-            primary_language = form.primary_language.data,
-            secondary_language = form.secondary_language.data,
-            previous_stay = form.stayed_before.data,
-            patient_first_name = form.patient_first_name.data,
-            patient_last_name = form.patient_last_name.data,
-            patient_dob = form.patient_dob.data,
-            patient_gender = form.patient_gender.data,
-            patient_hospital = form.hospital.data,
-            patient_hospital_department = form.hospital_dep.data,
-            patient_treatment_description = form.description.data,
-            patient_diagnosis = form.diagnosis.data,
-            patient_first_appt_date = form.first_appt_date.data,
-            patient_check_in = form.check_in_date.data,
-            patient_check_out = form.check_out_date.data,
-            patient_treating_doctor = form.treating_dr.data,
-            patient_doctors_phone = form.dr_phone_number.data,
-            patient_social_worker = form.hospital_social_worker.data,
-            patient_social_worker_phone = form.sw_phone_number.data,
-            inpatient = form.in_or_out_patient.data,
-            inpatient_prior = form.staying_prior_to_admission.data,
-            vaccinated = form.vaccinated.data,
-            comments = form.comments.data,
-            wheelchair_access = form.wheelchair_access.data,
-            full_bathroom = form.full_bathroom.data,
-            pack_n_play = form.pack_n_play.data
-        )
-        db.session.add(room_request)
-        db.session.commit()
-
-        from app.email import send_email
-        from flask_rq import get_queue
-        get_queue().enqueue(
-            send_email,
-            recipient=room_request.email,
-            subject='PRMH Room Request Submitted',
-            template='room_request/confirmation_email',
-            roomreq=room_request)
+        room_request = get_room_request_from_form(form)
+        try:
+            db.session.add(room_request)
+            db.session.commit()
+            get_queue().enqueue(
+                send_email,
+                recipient=room_request.email,
+                subject='PRMH Room Request Submitted',
+                template='room_request/confirmation_email',
+                roomreq=room_request)
+            flash('Successfully saved changes.', 'form-success')
+        except IntegrityError:
+            db.session.rollback()
+            flash('Unable to save changes. Please try again.', 'form-error')
         flash('Successfully submitted form', 'form-success')
-    return render_template('room_request/new_room_request.html', form=form, editable_html_obj=editable_html_obj)
-
-
-@login_required
-@room_request.route('<int:room_request_id>/delete', methods=['POST'])
-def delete_room_request(room_request_id):
-    """Request deletion of a user's account."""
-    room_request = RoomRequest.query.filter_by(id=room_request_id).first()
-    if room_request:
-        db.session.delete(room_request)
-        db.session.commit()
-        flash(f'Successfully deleted room request for {room_request.first_name} {room_request.last_name}.')
-    return redirect('/room-request/')
-
+    return render_template('room_request/new.html', form=form, editable_html_obj=editable_html_obj)
+    
 
 @login_required
 @room_request.route('/<int:id>', methods=['GET', 'POST'])
