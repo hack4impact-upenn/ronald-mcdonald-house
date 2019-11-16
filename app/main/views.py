a799014f429654c47453d16902bf80cdcc38fcc9
Katie Jiang <katiesjiang@gmail.com>
2019-11-16 16:45:20 -0500

    Remove about page (#23)

--- app/main/views.py
+++ app/main/views.py
@@ -8,10 +8,3 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('main/index.html')
-
-
-@main.route('/about')
-def about():
-    editable_html_obj = EditableHTML.get_editable_html('about')
-    return render_template(
-        'main/about.html', editable_html_obj=editable_html_obj)
