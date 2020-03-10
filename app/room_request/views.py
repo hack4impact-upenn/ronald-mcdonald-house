5a71b5592d96f88251480bbaf6e949daff703d41
grace jiang <gracjiang@gmail.com>
2020-03-10 18:17:52 -0400

    adding staff dashboard (#43)
    * staff dashboard
    
    * staff dashboard clicking on patients routing is broken, added to doc

--- app/room_request/views.py
+++ app/room_request/views.py
@@ -32,8 +32,10 @@ room_request = Blueprint('room_request', __name__)
 def index():
     """View all room requests."""
     room_requests = RoomRequest.query.all()
-    return render_template('admin/index.html', room_requests=room_requests)
-
+    if (current_user.role.name == 'Admin'):
+        return render_template('admin/index.html', room_requests=room_requests)
+    else:
+        return render_template('staff/index.html', room_requests=room_requests)
 
 @room_request.route('/<int:id>/manage')
 @room_request.route('/<int:id>/patient')
@@ -144,7 +146,10 @@ def _delete(id):
         db.session.delete(room_request)
         db.session.commit()
         flash(f'Successfully deleted room request for {room_request.first_name} {room_request.last_name}.', 'success')
-    return redirect(url_for('admin.index'))
+    if (current_user.role.name == 'Admin'):
+        return render_template('admin/index.html', room_requests=room_requests)
+    else:
+        return render_template('staff/index.html', room_requests=room_requests)
 
 
 @room_request.route('/new', methods=['GET', 'POST'])
